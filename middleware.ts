7d2aa7146798d302d233e47301628d9e310768ee
jakmlo <cucho1999k@gmail.com>
2024-01-25 20:23:51 +0100

    middleware fix

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,13 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|/|_next/image|favicon.ico|/api/auth/.*|/auth/.*|/password/reset).*)/",
-    // "/dashboard/:path*",
-    // "/rental/:path*",
-    // "/return/:path*",
-    // "/contractors/:path*",
-    // "/equipment/:path*",
-    // "/profile/:path*",
-    // "/workspaces/:path*",
+    // "/((?!api|_next/static|/|_next/image|favicon.ico|/api/auth/.*|/auth/.*|/password/reset).*)/",
+    "/dashboard/:path*",
+    "/rental/:path*",
+    "/return/:path*",
+    "/contractors/:path*",
+    "/equipment/:path*",
+    "/profile/:path*",
+    "/workspaces/:path*",
   ],
 };
