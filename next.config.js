7db6924336b5c2d2b77057f3acec17f4b18a845e
jakmlo <cucho1999k@gmail.com>
2024-01-28 15:58:09 +0100

    csp_rollback

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 
 const cspHeader = `
     default-src 'self';
-    script-src 'self';
-    style-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
