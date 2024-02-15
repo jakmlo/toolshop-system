13d8d93b76adb24ccc46344a17d1df596dc9f560
jakmlo <cucho1999k@gmail.com>
2024-02-15 11:07:42 +0100

    csp fix#2

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const cspHeader = `
     default-src 'self' files.edgestore.dev edge-store.s3.us-east-1.amazonaws.com;
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' files.edgestore.dev blob: data:;
+    img-src 'self' files.edgestore.dev localhost blob: data:;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
